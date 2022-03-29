e464b83d9b21471f624b75b2dc48dd154b2d329a
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-03-29 16:21:08 -0300

    Finalizacao do Desafio

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,9 @@
 package main;
 
+import br.mdan.desafio.Bootcamp;
 import br.mdan.desafio.Conteudo;
 import br.mdan.desafio.Curso;
+import br.mdan.desafio.Dev;
 import br.mdan.desafio.Mentoria;
 import java.time.LocalDate;
 
@@ -19,15 +21,51 @@ public class Main {
         curso2.setCargaHoraria(4);
         
         //Conteudo conteudo = new Curso();
+        //Conteudo conteudo1 = new Mentoria();
         
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Descrição Mentoria Java");
         mentoria.setData(LocalDate.now());
         
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+        
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+        
+        Dev devMarcio = new Dev();
+        devMarcio.setNome("Marcio");
+        devMarcio.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Marcio" + 
+                devMarcio.getConteudosInscritos());
+        devMarcio.progredir();
+        System.out.println("Conteúdos Inscritos Marcio" + 
+                devMarcio.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Marcio" + 
+                devMarcio.getConteudosConcluidos());
+        devMarcio.progredir();
+        System.out.println("XP:" + devMarcio.calcularTotalXp());
+        
+        System.out.println("----------------");
+        
+        Dev devDarlan = new Dev();
+        devMarcio.setNome("Darlan");
+        devDarlan.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Darlan" + 
+                devDarlan.getConteudosInscritos());
+        devDarlan.progredir();
+        System.out.println("Conteúdos Inscritos Darlan" + 
+                devDarlan.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Darlan" + 
+                devDarlan.getConteudosConcluidos());
+        System.out.println("XP:" + devDarlan.calcularTotalXp());
+        
     }
     
 }
